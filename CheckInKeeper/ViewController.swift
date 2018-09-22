c05fb92cccbe66108d4281cb06bca5da1595bad0
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-09-22 13:30:07 +0300

    Initial Commit

---
+++ CheckInKeeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckInKeeper
+//
+//  Created by Mihaela Simion on 9/22/18.
+//  Copyright © 2018 Mihaela Simion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
