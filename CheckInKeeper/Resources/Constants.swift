2afc6a051c0d76f12f7c6411a423389bfc8fbae5
Mihaela Simion <mihaelasimion2016@gmail.com>
2018-10-04 12:25:54 +0300

    added the 2 mail colors for future use

--- CheckInKeeper/Resources/Constants.swift
+++ CheckInKeeper/Resources/Constants.swift
@@ -6,10 +6,15 @@
 //  Copyright © 2018 Mihaela Simion. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Constants {
     struct ApiKeys {
         static let google = "AIzaSyB-1z-rG_KyG0mSI8nGAiHOTZA6EBMtQ4M"
+    }
+    
+    struct Colors {
+        static let backgroundColor = UIColor(red: 51, green: 89, blue: 113, alpha: 1.0)
+        static let buttonColor = UIColor(red: 67, green: 160, blue: 173, alpha: 1.0)
     }
 }
